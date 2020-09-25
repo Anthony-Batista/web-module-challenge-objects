4b39054d5076bc9aa83f9b98302b227eb94cb887
anthony-batista <a.batista0321@gmail.com>
2020-09-24 22:14:21 -0400

    finished mvp

--- index.js
+++ index.js
@@ -5,18 +5,16 @@ const burger = {name: "Burger", price: 18, category: "Lunch"};
 const breakfastBurrito = {name: "Breakfast Burrito", price: 16, category:"Breakfast"};
 
 /* Task 1a: write a function to return more menu items with the same format as the items above. */
-
 function createMenuItem(name, price, category){
-    
-    
+  return{name, price, category}
 }
 
 /* Task 1b: use your function to create 3 more menu items. You may add any items to the menu that you'd like */
 
 
-
-
-
+console.log(createMenuItem('cake', 4, 'dessert'))
+console.log(createMenuItem('ribs', 12, 'dinner'))
+console.log(createMenuItem('grilled cheese', 3, 'snack'))
 
 
 /* Task 2: You're having a lunch special! 25% off for teachers and students, 10% off for everyone else. Add a method to your burger object that automatically calculates price given a string as a parameter. 
@@ -30,20 +28,24 @@ and should return a number.
 For example, burger.discount("teacher") would return 13.5 and burger.discount("public") would return 16.2*/
 
 function discount(string) {
-  if (string == 'teacher' || string == 'student') {
-    console.log(burger.price-(burger.price*.25))
-  } else {
-    console.log(burger.price-(burger.price *.1))
-  }
+  // if (string == 'teacher' || string == 'student') {
+  //   console.log(burger.price-(burger.price*.25))
+  // } else {
+  //   console.log(burger.price-(burger.price *.1))
+  // }
+  string == 'teacher' || string == 'student' ? console.log (burger.price - (burger.price * .25)) :
+  console.log(burger.price - (burger.price * .1))
+
 }
-discount('student')
+
+discount('teacher')
 
 ///////////////Reviews (MVP)///////////////////
 
 const reviews = [{name: "Daniela", rating: 5, feedback:"Beautiful atmosphere and wonderful vegan options!"},
     {name: "Jack", rating: 3, feedback:"A little too hipster for my taste, but the burger was decent, if overpriced"},
     {name: "Miranda", rating: 4, feedback:"fun trivia and cool vibes"},
-    {name: "Wen", rating: 4.5, feedback:"I don't leave my house often, but when I do, it's for this place. Highly reccomend."},
+    {name: "Wen", rating: 4.5, feedback:"I don't leave my house often, but when I do, it's for this place. Highly recommend."},
     {name: "Brett", rating: 3, feedback: "great selection of snacks and a nice cafe area to get work done during the day."},
     {name: "Julius", rating: 2, feedback: "I was largely unimpressed by this venue. Nothing special on the menu and too expensive. The atmosphere is polarizing, and not for me, but I think some would like it." },
     {name:"Lauren", rating: 4, feedback: "Absolutely love that they have karaoke Fridays! Food and drink selection is okay."},
@@ -57,26 +59,16 @@ console.log(reviews[5].feedback)
 /* Task 4: Add a new rating with your (fictitious) opinions of the restaurant in the same format as the reviews above. */
 
 
-
-
-
-
-
-
-
-
+reviews.push({
+  name: 'Anthony',
+  rating: 4,
+  feedback: "Sometimes I like to bury myself in the dirt and pretend I'm a carrot"
+})
+console.log(reviews)
 
 /* Task 5: Add the following feedback to Reyna's rating - "this place is chill with really cool people, great for getting work done on weekdays" */
 
-
-
-
-
-
-
-
-
-
+reviews[7].feedback = 'This place is chill with really cool people, great for getting work done on weekdays'
 
 
 /*  Task 6: Write a function to return a review based on the index of the review in the array.
@@ -92,18 +84,18 @@ and should return a string in the format `{name} gave the restaurant a {rating},
  * For example, if getReviewByIndex is invoked with reviews and the number 0
  * it will return `Daniela gave the restaurant a 5 star review and their feedback was: Beautiful atmosphere and wonderful vegan options!`
 */
+
+
+// function getReviewByIndex(array, index) {
+//     console.log(`${array.index} gave the restaurant a ${this[index]}, and their feedback was: ${this[index]}`)
+//   }
+// getReviewByIndex(reviews, 3)
+
 function getReviewByIndex(reviews, index) {
-    /* code here */
-  }
-  
-
-
-
-
-
-
-
-
+  let customer = reviews[index]
+  return `${customer.name} gave the restaurant a ${customer.rating} and their feedback was : ${customer.feedback}`
+}
+console.log(getReviewByIndex(reviews, 0))
 
 
 
@@ -112,13 +104,22 @@ function getReviewByIndex(reviews, index) {
 getLastReview should accept:
   (1) an array of objects 
   
-and should return a string in the format `name} gave the restaurant a {rating}, and their feedback was: {feedback}`
+and should return a string in the format `${name} gave the restaurant a {rating}, and their feedback was: {feedback}`
 
 For example, if getLastReview is invoked passing the reviews array it will return `Reyna gave the restaurant a 3.5 star review and their feedback was: "this place is chill with really cool people, great for getting work done on weekdays"`.
 */
-function getLastReview(/* code here */) {
-    /* code here */
-  } 
+const getLastReview = (array) => {
+  const lastReview = [...array].pop()
+  return `${lastReview.name} gave the restaurant a ${lastReview.rating}, and their feedback was: ${lastReview.feedback}`
+}
+
+console.log(getLastReview(reviews))
+
+
+
+
+
+
 
 
 
@@ -151,7 +152,7 @@ function getLastReview(/* code here */) {
   and should return an array of objects. 
 
   For example, invoking getReviewByRating(reviews, 4) would return [{name: "Miranda", rating: 4, feedback:"fun trivia and cool vibes"},
-    {name: "Wen", rating: 4.5, feedback:"I don't leave my house often, but when I do, it's for this place. Highly reccomend."},
+    {name: "Wen", rating: 4.5, feedback:"I don't leave my house often, but when I do, it's for this place. Highly recommend."},
     {name:"Lauren", rating: 4, feedback: "Absolutely love that they have karaoke Fridays! Food and drink selection is okay."}]
 */
 
@@ -169,7 +170,7 @@ Your function should accept:
 and should return an array of objects. 
 
   For example, invoking getLongReviews(reviews) would return [
-    {name: "Wen", rating: 4.5, feedback:"I don't leave my house often, but when I do, it's for this place. Highly reccomend."},
+    {name: "Wen", rating: 4.5, feedback:"I don't leave my house often, but when I do, it's for this place. Highly recommend."},
     {name: "Brett", rating: 3, feedback: "great selection of snacks and a nice cafe area to get work done during the day."},
     {name: "Julius", rating: 2, feedback: "I was largely unimpressed by this venue. Nothing special on the menu and too expensive. The atmosphere is polarizing, and not for me, but I think some would like it." }]
 */
@@ -200,3 +201,4 @@ function carMaker(/* code here */) {
     /* code here */
     
 }
+
